02c5a5ad3dc8b39a214c70b8dfb24c785d95b1e4
Monika Leszczynska <leszczynska.monikaa@gmail.com>
2019-06-19 23:52:23 +0200

    initial commit

---
+++ mainjs.js
@@ -0,0 +1,7 @@
+console.log ('Siema!');
+const name=('Monika');
+const age=('31');
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+console.log('Witaj przybyszu!');
