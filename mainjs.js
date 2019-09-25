3752678412162f73af100635793eafdec76c1015
Monika Leszczynska <leszczynska.monikaa@gmail.com>
2019-09-25 10:41:38 +0200

    JS: modification

--- mainjs.js
+++ mainjs.js
@@ -5,3 +5,14 @@ console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
 console.log('Witaj przybyszu!');
+
+const box = document.querySelector('.box__paragraph--js');
+console.log(box.innerHTML);
+box.innerHTML=`Nazywam się ${name} i mam ${age} lat`;
+
+const paragraphs= document.querySelectorAll('p');
+console.log(paragraphs);
+console.log(paragraphs[5]);
+
+const about= document.querySelector('.box2__header--js');
+about.innerHTML=`Hello World!`;
