2e12489902ebee7754b03ab81ac91b4e1dcb4941
Monika Leszczynska <leszczynska.monikaa@gmail.com>
2020-02-13 00:02:06 +0100

    initial commit

---
+++ object.js
@@ -0,0 +1,11 @@
+ {
+    diameter: 120000,
+    "isOperating": true,
+    "levels": 357,
+    "name": 'Death Star',
+    "population": 10000,
+    "crew":{
+        "role": "captain",
+        "name": "deathstar captain"
+    }
+}
